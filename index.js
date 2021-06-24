544a731e00ba031ccc42af08bffd9359242bd5c4
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-06-24 22:56:23 +0200

    List in the cart done

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ fetch("https://striveschool-api.herokuapp.com/books")
     .then(res => res.json())
     .then(datas => {
         console.log(datas)
-        // myArr = datas;
+        myArr = datas;
         // console.log(myArr)
 
         const displayHtml = function () {
             let row = document.querySelector(".row")
-            datas.forEach(ele => {
+            myArr.forEach(ele => {
                 let col = document.createElement("div")
                 col.classList.add("col-12", "col-md-3", "mb-3", "d-flex")
                 col.insertAdjacentHTML(`afterbegin`, `<div class="card" style="width: 18rem;">
@@ -22,34 +22,51 @@ fetch("https://striveschool-api.herokuapp.com/books")
                 </div>
               </div>`)
               row.appendChild(col)
+
+              
             });
         }
         displayHtml()
 
-
+        
+        // console.log(datas[0].title)
         
 
-        let linkBtn = document.querySelectorAll("a")
+        let buttonList = document.querySelectorAll("a")      // grabing the list button links
         let badge = document.querySelector("span.badge")
         let count = 0;
-        linkBtn.forEach(elem => {
-          elem.addEventListener("click", function(){
-            let btnn = document.querySelector(".btn-primary")
-            btnn.classList.remove("btn-primary")
-            btnn.classList.add("btn-danger")
+
+        
+        
+        buttonList.forEach( button => {
+          button.addEventListener("click", function( event){
+            button.classList.remove("btn-primary")
+            button.classList.add("btn-danger")
+            let title = event.target.parentNode.childNodes[1];
+            title.closest("h5").style.color = "red"
             
+            console.log("event", event.target.parentNode.childNodes[1]);
+            console.log("button", button);
+            
+           
+
             count += 1;
-            // console.log(count)
+           
             badge.innerText = count
+            let list = document.querySelector("ul")
+            
+            list.insertAdjacentHTML(`afterbegin`, 
+              `<li class="list-group-item bg-primary w-50 d-flex justify-content-between">
+                <p>${title.innerText}</p>
+                <button type="button" class="btn btn-danger ml-5 remove">Remove</button>
+              </li>`
+            )
+
+            
+            
             
           })
         })
-
-
-        
-
-
-
 
     })
     .catch(err => console.log(err))
